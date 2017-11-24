a74637c12f496864fcb9db370b7bb9cc04a89f46
Rafa1390 <32964996+Rafa1390@users.noreply.github.com>
2017-11-24 11:44:44 -0600

    Add files via upload

---
+++ SegundoPrototipoProyecto/Prototipo2/src/main/java/com/contratacion/empleados/Empleado.java
@@ -0,0 +1,30 @@
+package com.contratacion.empleados;
+
+public abstract class Empleado {
+	String idEmpleado;
+	String nomEmpleado;
+	String apellEmpleado;
+	String telefono;
+	String email;
+	
+	public String getIdEmpleado() {
+		return idEmpleado;
+	}
+	public String getNomEmpleado() {
+		return nomEmpleado;
+	}
+	public String getApellEmpleado() {
+		return apellEmpleado;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	
+	public abstract String codPuesto();
+	public abstract String nomPuesto();
+	public abstract String departamento();
+	public abstract void encriptacion() throws Exception;
+}
